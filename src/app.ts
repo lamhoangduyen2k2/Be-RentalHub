d96b919e6411d76cb8ba242adca32e2e05db8be8
lamhoangduyen2k2 <lamhoangduyen9a4@gmail.com>
2024-05-21 15:15:18 +0700

    Update socket for sendMessage

--- src/app.ts
+++ src/app.ts
@@ -114,6 +114,7 @@ import { Server } from "socket.io";
       if (recipient) {
         io.to(recipient.socketId).emit("getMessage", message);
         io.to(recipient.socketId).emit("getUnreadMessage", {
+          chatId: message.chatId,
           senderId: message.senderId,
           isRead: false,
           date: new Date(),
