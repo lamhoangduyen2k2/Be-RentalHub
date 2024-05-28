18971e0d743925186729856ca59cfda104a96be1
lamhoangduyen2k2 <lamhoangduyen9a4@gmail.com>
2024-05-28 16:58:17 +0700

    Fix

--- src/app.ts
+++ src/app.ts
@@ -67,7 +67,7 @@ import cookieParser from "cookie-parser";
   app.use(express.urlencoded({ extended: true }));
   app.use(compression());
   app.use(helmet());
-  app.use(cors());
+  app.use(cors(corsOptions));
   app.use(morgan("combined"));
   app.use(cookieParser());
   app.set("view engine", "ejs");
